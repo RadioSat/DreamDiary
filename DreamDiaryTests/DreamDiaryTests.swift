3c04d1c3bb3af5b34e10cfc1e8d1bf261fca4c62
Saad El Maliki <saad.elmaliki@studio.unibo.it>
2025-06-19 18:34:59 +0200

    Initial Commit

---
+++ DreamDiaryTests/DreamDiaryTests.swift
@@ -0,0 +1,16 @@
+//
+//  DreamDiaryTests.swift
+//  DreamDiaryTests
+//
+//  Created by Saad El Maliki on 19/06/25.
+//
+
+import Testing
+
+struct DreamDiaryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
